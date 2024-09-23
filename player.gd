1c5e3ebe5cd4ff526945534ef88fa7f47d5f9879
Marcelo Cohen <mflashbr@gmail.com>
2024-09-23 17:53:10 -0300

    Exemplos de movimentação (final)

--- player.gd
+++ player.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 
 var rotation_direction = 0
 
+var target : Vector2 = position
+
 func get_8way_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 #	var dx = Input.get_action_strength("right")-Input.get_action_strength("left")
@@ -20,6 +22,17 @@ func get_rotation_input():
 	velocity = transform.x * Input.get_axis("down", "up") * speed
 	#print(transform.x)
 	
+func get_mouse_rotation():
+	look_at(get_global_mouse_position())
+	#print(transform.x)
+	velocity = transform.x * Input.get_axis("down", "up") * speed
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			target = get_global_mouse_position()
+			print(target)
+
 func _physics_process(delta: float) -> void:
 	# Descomente o bloco desejado de acordo com o tipo
 	# de movimento:
@@ -28,7 +41,18 @@ func _physics_process(delta: float) -> void:
 	#get_8way_input()
 	
 	# 2. Rotation + movement
-	get_rotation_input()
-	rotation += rotation_direction * rotation_speed * delta
+	#get_rotation_input()
+	#rotation += rotation_direction * rotation_speed * delta
 	
-	move_and_slide()
+	# 3. Mouse rotation + movement
+	#get_mouse_rotation()
+	
+	# 4. Mouse click + movement
+	velocity = position.direction_to(target) * speed
+	print(velocity)
+	#look_at(target)
+	if position.distance_to(target) > 5:
+		move_and_slide()
+	
+	# Descomente para todos os outros (1, 2 e 3)
+	#move_and_slide()
